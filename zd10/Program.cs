6780ecdc7111138b6070f1f6443cffdb23592a46
Robert <robert.kondr@gmail.com>
2023-06-14 11:06:12 +0700

    praktik2

---
+++ zd10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра числа -> "+stringNumber[1]);
---
+++ zd15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(ураааа, выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("такого дня не существует");
+  }
+  else Console.WriteLine("(этот день рабочий, быстро за работу!) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
